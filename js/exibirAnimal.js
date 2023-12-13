dfea57f6f36994357cb52881cb10de2c382afc2b
vinicius <viniciusmgomes2005@hotmail.com>
2023-12-13 18:01:14 -0300

    atualizando a horas e dias na exibição de gestante

--- js/exibirAnimal.js
+++ js/exibirAnimal.js
@@ -4,6 +4,9 @@ document.addEventListener("DOMContentLoaded", function() {
         exibirAnimais();
     }else if(urlAtual.includes('animaisGest.html')){
         exibirGestantes();
+        setInterval(function() {
+            exibirGestantes();
+        }, 5000);
     }else if(urlAtual.includes('animaisDoente')){
         exibirDoentes();
     }
@@ -94,8 +97,12 @@ function exibirGestantes(){
                 const idAnimal = document.createElement("p");
                 idAnimal.textContent = `ID: ${cadastrados.idAnimal}`;
 
-                const tempo = document.createElement("p");
-                tempo.textContent = `Tempo: ${cadastrados.tempoGest}`;
+                const tempoDias = document.createElement("p");
+                tempoDias.textContent = `Dias: ${cadastrados.tempoGest.dias}`;
+                const tempoHoras = document.createElement("p");
+                tempoHoras.textContent = `Horas: ${cadastrados.tempoGest.horas}`;
+                const tempoMinutos = document.createElement("p");
+                tempoMinutos.textContent = `Minutos: ${cadastrados.tempoGest.minutos}`;
 
                 const imagem = document.createElement("img");
                 imagem.src = cadastrados.imagem;
@@ -103,7 +110,9 @@ function exibirGestantes(){
 
                 animalContainer.appendChild(imagem);
                 animalContainer.appendChild(idAnimal);
-                animalContainer.appendChild(tempo);
+                animalContainer.appendChild(tempoDias);
+                animalContainer.appendChild(tempoHoras);
+                animalContainer.appendChild(tempoMinutos);
 
                 animal.appendChild(animalContainer);
             } 
@@ -128,6 +137,44 @@ function carregarDados() {
     return dadosExistente;
 }
 
+setInterval(function() {
+    atualizarTempoGestacao();
+}, 5000);
+
+function calcularDiferenca(dataInicio, dataAtual) {
+    const diferencaEmMilissegundos = dataAtual - dataInicio;
+    const diferencaEmMinutos = Math.floor(diferencaEmMilissegundos / 60000);
+    const dias = Math.floor(diferencaEmMinutos / (24 * 60));
+    const horas = Math.floor((diferencaEmMinutos % (24 * 60)) / 60);
+    const minutos = diferencaEmMinutos % 60;
+
+    return { dias, horas, minutos };
+}
+
+function atualizarTempoGestacao() {
+    const dados = carregarDados();
+
+    dados.forEach(animal => {
+        if (animal.tempoGest && typeof animal.tempoGest === 'object') {
+            console.log(`Atualizando tempo de gestação para o animal ${animal.idAnimal}`);
+            try {
+                const dataGestacao = new Date(animal.dataGestacao);
+                const diferencaAtualizada = calcularDiferenca(dataGestacao, new Date());
+
+                animal.tempoGest.dias = diferencaAtualizada.dias;
+                animal.tempoGest.horas = diferencaAtualizada.horas;
+                animal.tempoGest.minutos = diferencaAtualizada.minutos;
+
+                console.log(`Tempo de gestação atualizado: ${JSON.stringify(animal.tempoGest)}`);
+            } catch (error) {
+                console.error(`Erro ao atualizar o tempo de gestação para o animal ${animal.idAnimal}: ${error}`);
+                animal.tempoGest = null; 
+            }
+        }
+    });
+
+    localStorage.setItem('registroAnimal', JSON.stringify(dados));
+}
 
 function mudarTela(destino) {
     setTimeout(function() {
